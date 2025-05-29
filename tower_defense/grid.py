17db695a8bcedaa33265b62bf4b16aadaa4d4be2
Ignacy1 <iga.grz@op.pl>
2025-05-29 19:19:20 +0200

    grid.py

--- tower_defense/grid.py
+++ tower_defense/grid.py
@@ -7,10 +7,11 @@ def draw_grid(win, path_tiles, selected=None):
     for y in range(ROWS):
         for x in range(COLS):
             rect = pygame.Rect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE)
-            if (x, y) in path_tiles:
-                pygame.draw.rect(win, BROWN, rect)
-            elif selected == (x, y):
+            if selected == (x, y):
                 pygame.draw.rect(win, BLUE, rect)
+            elif (x, y) in path_tiles:
+                pygame.draw.rect(win, BROWN, rect)
+            """ najpierw zaznacza, aby wieżę 4 (barykadę) można było postawić w zaznaczonym miejscu na ścieżce """
             else:
                 pygame.draw.rect(win, GRAY, rect)
             pygame.draw.rect(win, DARK_GRAY, rect, 1)

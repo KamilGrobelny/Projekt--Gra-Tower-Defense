6846c7132203f2f4f8fdc0e5deed85fa318a14b2
Ignacy1 <iga.grz@op.pl>
2025-06-06 23:16:12 +0200

    settings.py

--- tower_defense/settings.py
+++ tower_defense/settings.py
@@ -25,3 +25,13 @@ BAR_HEIGHT = 5
 ENEMY_DATA = {'small':{'hp':50, 'speed':1.5, 'image':'images/enemy_small.png'},
                'normal':{'hp':100, 'speed':1, 'image':'images/enemy_normal.png'},
                'boss':{'hp':200, 'speed':0.4, 'image':'images/enemy_boss.png'}}
+
+TOWER_DATA = {1: {'range': 100, 'cooldown': 60, 'image': 'images/tower1.png', 'damage': 10},
+              2: {'range': 100, 'cooldown': 100, 'image': 'images/tower1.png', 'damage': 20},
+              3: {'range': 150, 'cooldown': 110, 'image': 'images/tower1.png', 'damage': 10},
+              4: {'range': 0, 'cooldown': 0, 'image': 'images/tower1.png', 'damage': 0, 'hp': 80}}
+
+TOWER_LEVEL_UP_DATA = {1: {'range': 100, 'cooldown': 50, 'image': 'images/tower2.png', 'damage': 10},
+                       2: {'range': 100, 'cooldown': 100, 'image': 'images/tower2.png', 'damage': 25},
+                       3: {'range': 150, 'cooldown': 130, 'image': 'images/tower2.png', 'damage': 10},
+                       4: {'range': 0, 'cooldown': 0, 'image': 'images/tower2.png', 'damage': 0, 'hp': 100}}

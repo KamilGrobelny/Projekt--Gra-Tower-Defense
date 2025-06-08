e27c924ab6131fed9e882297663566a0e911d353
AZushchyk <287702@student.pwr.edu.pl>
2025-06-08 02:16:53 +0200

    Add files via upload

---
+++ tower_defense/waves.py
@@ -0,0 +1,41 @@
+WAVES = {'Mapa 1':
+         
+    [
+    [('small', 6)],                                 
+    [('small', 6), ('normal', 2)],
+    [('normal', 6)],                              
+    [('small', 4), ('normal', 6)],
+    [('normal', 5), ('boss', 1)],               
+    [('small', 10)],                                
+    [('small', 5), ('normal', 8)],
+    [('normal', 7), ('boss', 2)],
+    [('small', 5), ('normal', 10), ('boss', 1)],     
+    [('normal', 10)],
+    [('normal', 6), ('boss', 3)],
+    [('boss', 5)],                                 
+    [('small', 6), ('normal', 6), ('boss', 3)],
+    [('normal', 5), ('boss', 6)],                   
+],                                 
+                
+
+        'Mapa 2':[
+    [('small', 5)],
+    [('small', 7)],
+    [('small', 5), ('normal', 2)],
+    [('small', 5), ('normal', 5)],
+    [('normal', 7)],
+    [('small', 3), ('normal', 7), ('boss', 1)],
+    [('normal', 8), ('boss', 1)],
+    [('small', 5), ('normal', 10), ('boss', 2)],
+    [('normal', 12), ('boss', 2)],
+    [('small', 5), ('normal', 10), ('boss', 3)],
+    [('normal', 10), ('boss', 4)],
+    [('small', 5), ('normal', 12), ('boss', 5)],
+    [('normal', 10), ('boss', 6)],
+    [('normal', 8), ('boss', 8)],
+    [('normal', 5), ('boss', 10)],
+        ],
+        'Test Mapa':[
+    [('small', 5)],            
+]
+}

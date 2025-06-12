ee0f8c782e88e53c8b7f4b5213f8808c027109f9
Ignacy1 <iga.grz@op.pl>
2025-06-12 22:51:14 +0200

    Update tower.py

--- tower_defense/tower.py
+++ tower_defense/tower.py
@@ -1,7 +1,7 @@
 import math
 import pygame
 
-from settings import TILE_SIZE, DARK_GRAY, RED, GREEN, BAR_WIDTH, BAR_HEIGHT, TOWER_DATA, TOWER_LEVEL_UP_DATA
+from settings import TILE_SIZE, DARK_GRAY, TOWER_DATA, TOWER_LEVEL_UP_DATA
 
 
 class Tower:
@@ -17,9 +17,6 @@ class Tower:
         self.orig_image = pygame.image.load(TOWER_DATA[name]['image'])
         self.image = pygame.transform.rotate(self.orig_image, self.angle)
         self.is_max_level = False
-        if name == 4:
-            self.hp = TOWER_DATA[name]['hp']
-            self.max_hp = TOWER_DATA[name]['hp']
 
     def draw(self, win):
         image = self.image.get_rect()
@@ -27,12 +24,6 @@ class Tower:
         image_y = self.y - image.height // 2
         win.blit(self.image, (image_x, image_y))
         pygame.draw.circle(win, DARK_GRAY, (self.x, self.y), self.range, 1)
-        if self.name == 4:
-            bar_x = self.x - BAR_WIDTH // 2
-            bar_y = self.y - 25
-            current_width = BAR_WIDTH * (self.hp / self.max_hp)
-            pygame.draw.rect(win, RED, (bar_x, bar_y, BAR_WIDTH, BAR_HEIGHT))
-            pygame.draw.rect(win, GREEN, (bar_x, bar_y, current_width, BAR_HEIGHT))
 
     def shoot(self, enemies):
         if self.timer > 0:
@@ -58,7 +49,28 @@ class Tower:
                 enemy.hp -= self.damage
             self.timer = self.cooldown
 
+        tower4_shots = False
+        
+        if self.name == 4:
+            enemies_for_tower4 = []
+            for enemy in enemies:
+                if math.hypot(enemy.x - self.x, enemy.y - self.y) <= 25:
+                    tower4_shots = True
+                
+            if tower4_shots:
+                for enemy in enemies:
+                    if math.hypot(enemy.x - self.x, enemy.y - self.y) <= self.range:
+                        enemies_for_tower4.append(enemy)
+
+                for enemy in enemies_for_tower4:
+                    enemy.hp -= self.damage
+
+        return tower4_shots
+
     def rotate(self, enemies):
+        if self.name == 4:
+            return
+
         for enemy in enemies:
             dist_x = enemy.x - self.x
             dist_y = enemy.y - self.y
@@ -76,6 +88,3 @@ class Tower:
         self.orig_image = pygame.image.load(TOWER_LEVEL_UP_DATA[self.name]['image'])
         self.image = pygame.transform.rotate(self.orig_image, self.angle)
         self.is_max_level = True
-        if self.name == 4:
-            self.hp = TOWER_LEVEL_UP_DATA[self.name]['hp']
-            self.max_hp = TOWER_LEVEL_UP_DATA[self.name]['hp']

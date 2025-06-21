279b0e81a86160b19cec49dbf29d65fa6cb33467
Ignacy1 <iga.grz@op.pl>
2025-06-21 14:22:16 +0200

    PEP8 i PEP257

--- tower_defense/tower.py
+++ tower_defense/tower.py
@@ -1,27 +1,37 @@
 import math
 import pygame
 
-from typing import Union
+from typing import Optional
 from enemy import Enemy
 from settings import TILE_SIZE, DARK_GRAY, TOWER_DATA, TOWER_LEVEL_UP_DATA
 
 
 class Tower:
-    def __init__(self, grid_x: int, grid_y: int, name: str) -> None:
-        self.name = name
+    """Klasa reprezentuje wieże.
+
+    Atrybuty:
+        tower_type: numer wieży
+        x: współrzędna x (środek pola)
+        y: współrzędna y (środek pola)
+    """
+
+    def __init__(self, grid_x: int, grid_y: int, tower_type: int) -> None:
+        """Inicjuje wieżę podanego typu i w odpowiednim miejscu."""
+        self.tower_type = tower_type
         self.x = grid_x * TILE_SIZE + TILE_SIZE // 2
         self.y = grid_y * TILE_SIZE + TILE_SIZE // 2
         self.timer = 0
-        self.range = TOWER_DATA[name]['range']
-        self.cooldown = TOWER_DATA[name]['cooldown']
-        self.damage = TOWER_DATA[name]['damage']
+        self.range = TOWER_DATA[tower_type]['range']
+        self.cooldown = TOWER_DATA[tower_type]['cooldown']
+        self.damage = TOWER_DATA[tower_type]['damage']
         self.angle = 0
-        self.orig_image = pygame.image.load(TOWER_DATA[name]['image'])
+        self.orig_image = pygame.image.load(TOWER_DATA[tower_type]['image'])
         self.image = pygame.transform.rotate(self.orig_image, self.angle)
         self.is_max_level = False
 
     
     def draw(self, win: pygame.Surface) -> None:
+        """Rysuje wieżę oraz zasięg."""
         image = self.image.get_rect()
         image_x = self.x - image.width // 2
         image_y = self.y - image.height // 2
@@ -29,13 +39,14 @@ class Tower:
         pygame.draw.circle(win, DARK_GRAY, (self.x, self.y), self.range, 1)
 
     
-    def shoot(self, enemies: Enemy) -> Union[bool, None]:
+    def shoot(self, enemies: list[Enemy]) -> Optional[bool]:
+        """Strzela do wrogów znajdujących się w zasięgu."""
         if self.timer > 0:
             self.timer -= 1
             return
 
         for enemy in enemies:
-            if self.name == 3 or self.name == 4:
+            if self.tower_type == 3 or self.tower_type == 4:
                 break
             dist = math.hypot(enemy.x - self.x, enemy.y - self.y)
             if dist <= self.range:
@@ -43,7 +54,7 @@ class Tower:
                 self.timer = self.cooldown
                 break
         
-        if self.name == 3:
+        if self.tower_type == 3:
             enemies_for_tower3 = []
             for enemy in enemies:
                 if math.hypot(enemy.x - self.x, enemy.y - self.y) <= self.range:
@@ -55,7 +66,7 @@ class Tower:
 
         tower4_shots = False
         
-        if self.name == 4:
+        if self.tower_type == 4:
             enemies_for_tower4 = []
             for enemy in enemies:
                 if math.hypot(enemy.x - self.x, enemy.y - self.y) <= 25:
@@ -73,7 +84,8 @@ class Tower:
 
     
     def rotate(self, enemies: list[Enemy]) -> None:
-        if self.name == 4:
+        """Obraca wieżę w stronę wroga."""
+        if self.tower_type == 4:
             return
 
         for enemy in enemies:
@@ -87,10 +99,11 @@ class Tower:
 
     
     def level_up(self) -> None:
-        self.range = TOWER_LEVEL_UP_DATA[self.name]['range']
-        self.cooldown = TOWER_LEVEL_UP_DATA[self.name]['cooldown']
-        self.damage = TOWER_LEVEL_UP_DATA[self.name]['damage']
+        """Ulepsza wieżę."""
+        self.range = TOWER_LEVEL_UP_DATA[self.tower_type]['range']
+        self.cooldown = TOWER_LEVEL_UP_DATA[self.tower_type]['cooldown']
+        self.damage = TOWER_LEVEL_UP_DATA[self.tower_type]['damage']
         self.angle = 0
-        self.orig_image = pygame.image.load(TOWER_LEVEL_UP_DATA[self.name]['image'])
+        self.orig_image = pygame.image.load(TOWER_LEVEL_UP_DATA[self.tower_type]['image'])
         self.image = pygame.transform.rotate(self.orig_image, self.angle)
         self.is_max_level = True

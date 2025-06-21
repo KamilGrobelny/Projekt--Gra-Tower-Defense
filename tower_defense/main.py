7b9e4423a78ee789aafab2e07ea29ec08aa9f9f5
Ignacy1 <iga.grz@op.pl>
2025-06-21 14:58:32 +0200

    PEP8 i PEP257

--- tower_defense/main.py
+++ tower_defense/main.py
@@ -8,13 +8,13 @@ from game import game_loop
 
 
 def main():
+    """Główna pętla programu, która umożliwia wybór trybu oraz mapy gry."""
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
     pygame.init()
     win = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("Tower Defense")
 
-    new_game = True
-    while new_game:
+    while True:
         mode = choose_mode(win)
         if not mode:
             break
@@ -23,7 +23,7 @@ def main():
         if not path:
             break
 
-        new_game = game_loop(win, path, mode)
+        game_loop(win, path, mode)
 
     pygame.quit()
 

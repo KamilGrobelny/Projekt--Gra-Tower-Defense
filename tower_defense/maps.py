cea55466a078831766ebde28cb811fb05d7ded72
AZushchyk <287702@student.pwr.edu.pl>
2025-06-21 17:04:43 +0200

    Update maps.py

--- tower_defense/maps.py
+++ tower_defense/maps.py
@@ -24,6 +24,26 @@ MAPS = {
         + [(x, 11) for x in range(8, 18)]
         + [(18, 12), (19, 13)]
     ),
-    "Test Mapa":
+    "Mapa 3": (
         [(x, 2) for x in range(5)]
+        + [(4,y) for y in range(2,7)]
+        + [(x, 6) for x in range(4,0,-1)]
+        + [(1,y) for y in range(6,3,-1)]
+        + [(x, 4) for x in range(1,19)]
+        + [(18,y) for y in range(4,7)]
+        + [(x, 6) for x in range(18,12,-1)]
+        + [(13,y) for y in range(6,1,-1)]
+        + [(x, 2) for x in range(13,7,-1)]
+        + [(8,y) for y in range(2,10)]
+        + [(x, 9) for x in range(8,1,-1)]
+        + [(2,y) for y in range(9,13)]
+        + [(x, 12) for x in range(2,7)]
+        + [(6,y) for y in range(12,6,-1)]
+        + [(x, 7) for x in range(6,12)]
+        + [(11,y) for y in range(7,12)]
+        + [(x, 11) for x in range(11,17)]
+        + [(16,y) for y in range(11,8,-1)]
+        + [(x, 9) for x in range(16,19)]
+        + [(18,10),(18,11),(19,11)]
+    )
     }
